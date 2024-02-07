04f9f2084ae0173f07a58011b4c7b50364ccdd82
LucaslcFreitas <lucaslcfjf@hotmail.com>
2024-02-07 16:24:39 -0300

    Ajustes iniciais e configuração do styled-components

---
+++ src/app/home.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import styled from "styled-components";
+
+const Heading = styled.h1`
+  background: ${({ theme }) => theme.colors.primaryColor};
+`;
+
+export default function Homepage() {
+  return (
+    <main>
+      <Heading>Portfolio Heading</Heading>
+    </main>
+  );
+}
