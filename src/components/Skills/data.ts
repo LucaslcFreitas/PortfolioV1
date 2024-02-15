ed626baf338f1397442a3fb15f050ffd3432b45f
Lucas Freitas <lucaslcfjf@hotmail.com>
2024-02-15 11:47:36 -0300

    Criação dos ícones das skills e motagem da listagem dos dados

---
+++ src/components/Skills/data.ts
@@ -0,0 +1,134 @@
+import { SkillList } from '.';
+
+export const skillsData: SkillList[] = [
+    {
+        id: '1',
+        label: 'React js',
+        imgSource: './assets/skills_icons/reactjs.png',
+    },
+    {
+        id: '2',
+        label: 'React Native',
+        imgSource: './assets/skills_icons/reactnative.png',
+    },
+    {
+        id: '3',
+        label: 'Angular',
+        imgSource: './assets/skills_icons/angular.png',
+    },
+    {
+        id: '4',
+        label: 'Next js',
+        imgSource: './assets/skills_icons/nextjs.png',
+    },
+    {
+        id: '5',
+        label: 'HTML5',
+        imgSource: './assets/skills_icons/html5.png',
+    },
+    {
+        id: '6',
+        label: 'CSS3',
+        imgSource: './assets/skills_icons/css3.png',
+    },
+    {
+        id: '7',
+        label: 'JavaScript',
+        imgSource: './assets/skills_icons/javascript.png',
+    },
+    {
+        id: '8',
+        label: 'TypeScript',
+        imgSource: './assets/skills_icons/typescript.png',
+    },
+    {
+        id: '9',
+        label: 'Git',
+        imgSource: './assets/skills_icons/git.png',
+    },
+    {
+        id: '10',
+        label: 'GitHub',
+        imgSource: './assets/skills_icons/github.png',
+    },
+    {
+        id: '11',
+        label: 'Axios',
+        imgSource: './assets/skills_icons/axios.png',
+    },
+    {
+        id: '12',
+        label: 'SASS',
+        imgSource: './assets/skills_icons/sass.png',
+    },
+    {
+        id: '13',
+        label: 'Styled Components',
+        imgSource: './assets/skills_icons/styledcomponents.png',
+    },
+    {
+        id: '14',
+        label: 'Prettier',
+        imgSource: './assets/skills_icons/prettier.png',
+    },
+    {
+        id: '15',
+        label: 'ESLint',
+        imgSource: './assets/skills_icons/eslint.png',
+    },
+    {
+        id: '16',
+        label: 'Redux',
+        imgSource: './assets/skills_icons/redux.png',
+    },
+    {
+        id: '17',
+        label: 'Figma',
+        imgSource: './assets/skills_icons/figma.png',
+    },
+    {
+        id: '18',
+        label: 'Docker',
+        imgSource: './assets/skills_icons/docker.png',
+    },
+    {
+        id: '19',
+        label: 'Jest',
+        imgSource: './assets/skills_icons/jest.png',
+    },
+    {
+        id: '20',
+        label: 'Testing Library',
+        imgSource: './assets/skills_icons/testinglibrary.png',
+    },
+    {
+        id: '21',
+        label: 'Node js',
+        imgSource: './assets/skills_icons/node.png',
+    },
+    {
+        id: '22',
+        label: 'Express',
+        imgSource: './assets/skills_icons/express.png',
+    },
+    {
+        id: '23',
+        label: 'TypeORM',
+        imgSource: './assets/skills_icons/typeorm.png',
+    },
+    {
+        id: '24',
+        label: 'Prisma',
+        imgSource: './assets/skills_icons/prisma.png',
+    },
+    {
+        id: '25',
+        label: 'PostgreSQL',
+        imgSource: './assets/skills_icons/postgresql.png',
+    },
+    {
+        id: '26',
+        label: 'MongoDB',
+        imgSource: './assets/skills_icons/mongodb.png',
+    },
+];
