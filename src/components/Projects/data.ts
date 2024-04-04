6b9bf158028315a9a54dd8f696127e8665a53dd3
LucaslcFreitas <lucaslcfjf@hotmail.com>
2024-04-04 16:46:34 -0300

    Adiciona mock de imagem para os projetos e monta listagem

---
+++ src/components/Projects/data.ts
@@ -0,0 +1,160 @@
+import { ProjectType } from '.';
+
+export const projectsData: ProjectType[] = [
+    {
+        id: '1',
+        name: 'Best Jobs',
+        thumbnail: './assets/projects/mock.png',
+        createAt: '',
+        descriptions: ['Projeto Best Jobs'],
+        repositoryUrl: 'https://github.com/LucaslcFreitas/BestJobs',
+        viewUrl: '',
+        images: [
+            {
+                id: '1',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '2',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '3',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '4',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '5',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '6',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '7',
+                imgSource: './assets/projects/mock.png',
+            },
+        ],
+    },
+    {
+        id: '2',
+        name: 'Best Jobs',
+        thumbnail: './assets/projects/mock.png',
+        createAt: '',
+        descriptions: ['Projeto Best Jobs'],
+        repositoryUrl: 'https://github.com/LucaslcFreitas/BestJobs',
+        viewUrl: '',
+        images: [
+            {
+                id: '1',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '2',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '3',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '4',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '5',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '6',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '7',
+                imgSource: './assets/projects/mockpng',
+            },
+        ],
+    },
+    {
+        id: '3',
+        name: 'Best Jobs',
+        thumbnail: './assets/projects/mock.png',
+        createAt: '',
+        descriptions: ['Projeto Best Jobs'],
+        repositoryUrl: 'https://github.com/LucaslcFreitas/BestJobs',
+        viewUrl: '',
+        images: [
+            {
+                id: '1',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '2',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '3',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '4',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '5',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '6',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '7',
+                imgSource: './assets/projects/mock.png',
+            },
+        ],
+    },
+    {
+        id: '4',
+        name: 'Best Jobs',
+        thumbnail: './assets/projects/mock.png',
+        createAt: '',
+        descriptions: ['Projeto Best Jobs'],
+        repositoryUrl: 'https://github.com/LucaslcFreitas/BestJobs',
+        viewUrl: '',
+        images: [
+            {
+                id: '1',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '2',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '3',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '4',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '5',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '6',
+                imgSource: './assets/projects/mock.png',
+            },
+            {
+                id: '7',
+                imgSource: './assets/projects/mock.png',
+            },
+        ],
+    },
+];
