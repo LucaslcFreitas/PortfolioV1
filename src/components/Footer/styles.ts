fd259fe2200733a096e3d3de4fb4083c574b84f3
LucaslcFreitas <lucaslcfjf@hotmail.com>
2024-04-05 20:36:44 -0300

    Adição de responsividade no rodapé (footer)

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -2,10 +2,31 @@ import styled from 'styled-components';
 
 export const FooterContainer = styled.footer`
     position: relative;
-    width: 130rem;
+    width: ${({ theme }) => theme.container.large};
     margin: 7rem auto;
     padding: 4rem;
     border-top: 0.2rem solid ${({ theme }) => theme.colors.backgroundTertiary};
+
+    @media ${({ theme }) => theme.media.smallMobile} {
+        width: ${({ theme }) => theme.container.smallMobile};
+        padding: 4rem 2rem;
+    }
+
+    @media ${({ theme }) => theme.media.mobile} {
+        width: ${({ theme }) => theme.container.mobile};
+    }
+
+    @media ${({ theme }) => theme.media.tablet} {
+        width: ${({ theme }) => theme.container.tablet};
+    }
+
+    @media ${({ theme }) => theme.media.smallDesktop} {
+        width: ${({ theme }) => theme.container.small};
+    }
+
+    @media ${({ theme }) => theme.media.normalDesktop} {
+        width: ${({ theme }) => theme.container.medium};
+    }
 `;
 
 export const FooterText = styled.p`
